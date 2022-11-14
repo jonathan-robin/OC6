c2c192a1583551617dd28bf2fc72c95d9c387ed4
ROBIN Jonathan <jonathan@levelup-analytics.com>
2022-11-14 15:49:37 +0400

    init login routes

--- controller/user.js
+++ controller/user.js
@@ -4,19 +4,28 @@ const User = require('../models/user');
 
 
 exports.signup = async (req, res, next) => { 
-    console.log(req);
-    console.log(req.body);
+    // get the request payload
     let passTmp = req.body.password; 
-
-    let salt = await bcrypt.genSalt(10);
-    let password = await bcrypt.hash(passTmp, salt);
-
     let email = req.body.email; 
 
+    // hash pass
+    let salt = await bcrypt.genSalt(10);
+    let password = await bcrypt.hash(passTmp, salt);
+    
+    let user = new User({email, password}); // create new user
+    user.save() // then save it 
+    .then(data => res.status(201).send(data)) // if no error send back 201 status code
+    .catch((err) => { // catch err if code: 11000 -> it means email is dupplicate 
+        return err.code === 11000 ? res.status(500).send('Email is already in use.') : res.status(500).send('Impossible de créer le compte.')
+    });
+}
 
-    let user = new User({email, password});
+exports.login = async (req, res, next) => {
+    // get the request payload
+    let email = req.body.email; 
+    let password = req.body.password; 
 
-    user.save()
-    .then(data => res.status(201).send(data))
-    .catch(err => console.log(err));
+    
+
+
 }
--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const userController = require('../controller/user'); 
 
 router.post('/signup', userController.signup); 
-// router.post('login', userController.login);
+router.post('/login', userController.login);
 
 module.exports = router;
 
