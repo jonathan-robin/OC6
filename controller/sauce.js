db56690fd3141ff4df560545c2b780a55ef8bcf2
ROBIN Jonathan <jonathan@levelup-analytics.com>
2022-12-26 12:22:09 +0400

    bugfix

--- controller/sauce.js
+++ controller/sauce.js
@@ -12,15 +12,14 @@ exports.addSauce = (req, res, next) => {
 // Update a sauce
 exports.updateSauce = async (req, res, next) => { 
     // get former data sauce with the id
-    let formerSauce = await Sauce.findOne({ _id: req.params.id })
-    // .then(res => res); 
+    let formerSauce = await Sauce.findOne({ _id: req.params.id }).then()
     let sauceTmp = req.file ? JSON.parse(req.body.sauce) : req.body; 
     req.file ? sauceTmp.imageUrl = 'http://localhost:3000/public/'+req.file.filename : null;
     // initialize for sauce updates
     let updatesMessage = '';
     // compare which entries has changed and push into string
     Object.entries(sauceTmp).forEach(([key,value]) => updatesMessage += sauceTmp[key] != formerSauce[key] ? `${key}, ` : '')
-    Sauce.updateOne({ _id: req.params.id }, sauceTmp).then(() => res.status(200).json({ message: `Sauce id: ${ req.params.id} updated with ${updatesMessage.slice(0, -2)}.` })).catch(error => res.status(400).json({ error }))
+    Sauce.updateOne({ _id: req.params.id }, sauceTmp).then(res.status(200).json({ message: `Sauce id: ${ req.params.id} updated with ${updatesMessage.slice(0, -2)}.`})).catch(error => res.status(400).json({ error }))
 }
 // Delete a sauce in db and the image in directory
 exports.deleteSauce = (req, res, next) => { 
@@ -38,7 +37,7 @@ exports.rateSauce = (req, res, next) => {
             return Sauce.updateOne({ _id: req.params.id },{$inc: {likes: req.body.like++}, $push:{usersLiked: req.body.userId}}).then(res.status(200).json('+1 like')).catch(error => json({ error }))
         case -1: // -1 mean add 1 dislike
              // retrieve the sauce with id and increment the dislikes +1 and push into usersDisliked array the userId
-            return Sauce.updateOne({ _id: req.params.id },{ $inc: {dislikes: req.body.like++}, $push: {usersDisliked: req.body.userId}}).then(res.status(200).json('+1 dislike')).catch(error => json({ error }))
+            return Sauce.updateOne({ _id: req.params.id },{ $inc: {dislikes: req.body.like++*-1}, $push: {usersDisliked: req.body.userId}}).then(res.status(200).json('+1 dislike')).catch(error => json({ error }))
         default : // 0 means the user cancel its rate
             return Sauce.findOne({ _id: req.params.id }).then(sauce => { // retrieve sauce with id
                 // if the userLike array include user id pull userId from userLike array and incr by -1 the total likes and vice versa
